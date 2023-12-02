f958796407abaf103dfadff2be389df5c182456c
unknown <hosseind600@gmail.com>
2023-12-03 01:46:24 +0330

    Added new Search Modal component

---
+++ dist/js/about.7b598219.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdxboffplan"]=self["webpackChunkdxboffplan"]||[]).push([[443],{4007:function(n,a,u){u.r(a),u.d(a,{default:function(){return l}});var t=u(3396);const e={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function f(n,a){return(0,t.wg)(),(0,t.iD)("div",e,s)}var c=u(89);const o={},i=(0,c.Z)(o,[["render",f]]);var l=i}}]);
+//# sourceMappingURL=about.7b598219.js.map
